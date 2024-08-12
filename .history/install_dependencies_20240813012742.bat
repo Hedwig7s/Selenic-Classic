3877c55fdd5aa89576f078b025697a8f6f895296
Hedwig7s <51961150+Hedwig7s@users.noreply.github.com>
2024-08-13 01:28:14 +0200

    Add Windows install script
    e

---
+++ .history/install_dependencies_20240813012742.bat
@@ -0,0 +1,5 @@
+sudo luarocks install lua-zlib
+sudo luarocks install toml
+sudo luarocks install bit32
+sudo luarocks install compat53
+sudo luarocks install md5
---
+++ install_dependencies.bat
@@ -0,0 +1,5 @@
+luarocks install lua-zlib
+luarocks install toml
+luarocks install bit32
+luarocks install compat53
+luarocks install md5
