028e3e0450114466df1a17c55ff0041ec8056682
Hedwig7s <51961150+Hedwig7s@users.noreply.github.com>
2024-08-16 12:47:20 +0200

    Add salt caching

--- server.lua
+++ server.lua
@@ -1,27 +1,53 @@
 ---@class Server
 local module = {}
 
+local fs = require("fs")
+local util = require("./util")
+
+local salt = ""
+for i=1, 32 do
+    local t = math.random(1,3)
+    local num
+    if t == 1 then
+        num = math.random(48,57)
+    elseif t == 2 then
+        num = math.random(65,90)
+    else
+        num = math.random(97,122)
+    end
+    salt = salt .. string.char(num)
+end
+
+local function writeSalt()
+    fs.writeFileSync("cachedSalt.txt", salt..":"..os.time())
+end
+
+if fs.existsSync("cachedSalt.txt") then
+    local success, err = pcall(function()
+        local saltData = util.split(fs.readFileSync("cachedSalt.txt"),":")
+        local oldSalt, time = saltData[1], saltData[2]
+        if not time or not oldSalt then
+            error("Salt data invalid")
+        end
+        time = tonumber(time)
+        if not time then
+            error("Salt time invalid")
+        end
+        if os.time() - time < 600 then
+            salt = oldSalt
+        end
+    end)
+    if not success and err then
+        print("Error reading salt data: "..err) -- Not critical
+    end
+end
+writeSalt()
 ---@class ServerInfo
 module.info = {
-    Version = "v0.5.2-alpha",
+    Version = "v0.5.3-alpha",
     Software = "Selenic Classic",
     Source = "https://github.com/Hedwig7s/Selenic-Classic",
-    Salt = (function ()
-        local salt = ""
-        for i=1, 16 do
-            local t = math.random(1,3)
-            local num
-            if t == 1 then
-                num = math.random(48,57)
-            elseif t == 2 then
-                num = math.random(65,90)
-            else
-                num = math.random(97,122)
-            end
-            salt = salt .. string.char(num)
-        end
-        return salt
-    end)()
+    Salt = salt
 }
 
 return module
