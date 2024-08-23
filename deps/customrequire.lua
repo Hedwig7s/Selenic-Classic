0b9c0bf21589d2abc6870e6b62942d48fdf54e4c
Hedwig7s <51961150+Hedwig7s@users.noreply.github.com>
2024-08-23 23:00:19 +0200

    Work on infrastructure

--- deps/customrequire.lua
+++ deps/customrequire.lua
@@ -1,32 +1,36 @@
 local currentRequire = require
-local tluvit = currentRequire('tluvit')
+--local tluvit = currentRequire('tluvit')
 local fs = currentRequire('fs')
 local pathModule = currentRequire('path')
 
-local modulePath = "./"
+local modulePath = "./src"
 
 local function toPath(module)
-    local cachedSuffix = ""
+    --[[local cachedSuffix = ""
     if module:sub(-5) == ".d.tl" then
         module = module:sub(1, -6)
         cachedSuffix = ".d.tl"
     elseif module:sub(-3) == ".tl" then
         module = module:sub(1, -4)
         cachedSuffix = ".tl"
-    end
-    return pathModule.resolve(modulePath..module:gsub('%.', '/')..cachedSuffix)
+    end]]
+    module = module:gsub('%.', '/')
+    return pathModule.resolve(pathModule.join(modulePath, module))--..cachedSuffix)
 end
+
+local types = {"lua", package.cpath:match("%p[\\|/]?%p(%a+)")}
 
 local function customRequire(module)
     local path = toPath(module)
-    local searchPath = pathModule.dirname(path)
-    local file = pathModule.basename(path)
-    if fs.existsSync(searchPath.."/"..file..".tl") then
-        return tluvit.loadtl(searchPath.."/"..file..".tl")
-    elseif fs.existsSync(searchPath.."/"..file..".lua") then
-        return currentRequire(searchPath.."/"..file..".lua")
+   -- local searchPath = pathModule.dirname(path)
+    --local file = pathModule.basename(path)
+    --if fs.existsSync(searchPath.."/"..file..".tl") then
+        --return tluvit.loadtl(searchPath.."/"..file..".tl")
+    for _, t in pairs(types) do
+        if fs.existsSync(path.."."..t) then
+            return currentRequire(path.."."..t)
+        end
     end
-
     return currentRequire(module)
 end
 
