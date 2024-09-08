f977a32395a17902cad24fa0ec9b8dcc19951d75
Hedwig7s <51961150+Hedwig7s@users.noreply.github.com>
2024-09-08 19:06:54 +0200

    Player spawning and move logging to lualogging

--- main.lua
+++ main.lua
@@ -8,14 +8,38 @@ local Vector3 = require("datatypes/vector3")
 local timer = require("timer")
 
 logger:Info("Loading config...")
-serverConfig:loadFromFile()
-loggerClass.globalSettings.DEBUG = serverConfig:get("server.debug")
+
+do
+    local success, err = pcall(function()
+        serverConfig:loadFromFile()
+        loggerClass.globalSettings.DEBUG = serverConfig:get("server.debug")
+    end)
+    if not success then
+        logger:Fatal("Failed to load config: " .. err)
+    end
+end
 
 logger:Info("Loading worlds...")
-worldModule:loadOrCreate(serverConfig:get("server.defaultWorld"), "hworld", Vector3.new(512, 128, 512))
-timer.setInterval(20000, worldModule.saveAll)
+
+do
+    local success, err = pcall(function()
+        worldModule:loadOrCreate(serverConfig:get("server.defaultWorld"), "hworld", Vector3.new(512, 128, 512))
+        timer.setInterval(20000, worldModule.saveAll)
+    end)
+    if not success then
+        logger:Fatal("Failed to load worlds: " .. err)
+    end
+end
+
 
 logger:Info("Starting server...")
 
-local server = serverClass:new("0.0.0.0", 25565)
-server:init()
+do 
+    local success, err = pcall(function()
+        local server = serverClass:new("0.0.0.0", 25565)
+        server:init()
+    end)
+    if not success then
+        logger:Fatal("Fatal error occured while running server: " .. err)
+    end
+end
