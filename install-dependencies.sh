92c30786e5de8f6cc26b9d266543f5333f5a61c3
Hedwig7s <51961150+Hedwig7s@users.noreply.github.com>
2024-10-13 23:16:06 +0200

    Migrate from tl 0.15 to 0.24 (part 1)

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -4,3 +4,4 @@ luarocks install cyan --tree rocks
 luarocks install lua-zlib --tree rocks
 luarocks install toml --tree rocks 
 luarocks install md5 --tree rocks
+luarocks install luassert --tree rocks
